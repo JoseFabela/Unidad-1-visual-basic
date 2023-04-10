f8af76193e5aa4dc47a55af84199f9dfc824a703
JoseFabela <jfabela522@gmail.com>
2023-04-10 17:54:00 -0500

    Agregar archivos de proyecto.

---
+++ Unidad 1 visual basic/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+    Sub Main()
+        ' Planificación
+        Console.WriteLine("Definir objetivos del proyecto")
+
+        ' Diseño
+        Console.WriteLine("Crear diseño detallado")
+
+        ' Implementación
+        Console.WriteLine("Escribir código")
+
+        ' Pruebas
+        Console.WriteLine("Realizar pruebas")
+
+        ' Mantenimiento
+        Console.WriteLine("Realizar actualizaciones")
+    End Sub
+End Module
